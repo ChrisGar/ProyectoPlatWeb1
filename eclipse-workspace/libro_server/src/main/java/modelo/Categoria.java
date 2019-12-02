55431010f0fad3df892aca5b47b9d06d3b4397f0
Joel Vasquez <Joel Vasquez@JoelVasquez>
2019-12-02 13:35:38 -0500

    Relacion libro-categoria

--- eclipse-workspace/libro_server/src/main/java/modelo/Categoria.java
+++ eclipse-workspace/libro_server/src/main/java/modelo/Categoria.java
@@ -2,39 +2,44 @@ package modelo;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria implements Serializable{
+public class Categoria {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4673459807906818530L;
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private String codigo;
+	
+	@Column(name="nombre")
 	private String nombre;
-	
-	
+
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	@Override
 	public String toString() {
 		return "Categoria [codigo=" + codigo + ", nombre=" + nombre + "]";
 	}
-	
-	
-	
-	
 }
